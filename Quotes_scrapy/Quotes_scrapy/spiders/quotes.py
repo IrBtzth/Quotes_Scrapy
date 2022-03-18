47665f757ae94c1a6ab457b54becd5109e9af78a
betza <betzabeth.btzth@gmail.com>
2022-03-18 09:41:26 -0400

    El reto esta listo pero no corre

--- Quotes_scrapy/Quotes_scrapy/spiders/quotes.py
+++ Quotes_scrapy/Quotes_scrapy/spiders/quotes.py
@@ -1,5 +1,6 @@
 import scrapy 
 
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
 class MySpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -8,27 +9,65 @@ class MySpider(scrapy.Spider):
     
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
-        }
+        'FEED_FORMAT': 'json',
+        'CURRRENT_REQUESTS': 24, #Como scrapy es un framework asincrono puede mantener varias peticiones a la vez, sin la necesidad de terminar una para comenzar la otra
+        'MEMUSAGE_LIMIT_MB': 2048, #la cantidad de memoria ram que scrapy puede utilizar.
+        'MEMUSAGE_NOTIFY_MAIL': ['betzabeth.btzth@gmail.com'], #nos avisa cuando la memoria ram se llena o llega al limite
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'Un ente', #en el sevidor cuando llegue la peticion dira que la hizo un ente
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+    def parse_only_quotes(self, response, **kwargs):
         
+        if kwargs:
+            quotes = kwargs['quotes'] #Aqui se guardan las citas, inicialmente vacia, es una lista
+        quotes.extend(response.xpath(
+            "//span[@class = 'text' and @itemprop= 'text']/text()").getall()) #aqui se agregan
 
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, 
+            callback=self.parse_only_quotes, cb_kwargs= {'quotes': quotes})
+        else:
+            authors = response.xpath('//small[@class="author" and @itemprop = "author"]/text()').getall()
+            for i in range(0, len(quotes)-1):
+                quotes[i]= quotes[i]+ " by "+ authors[i]
+            yield {
+                'quotes': quotes
+                }
+    
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
   
-        citas = response.xpath("//span[@class = 'text' and @itemprop= 'text']/text()").getall()
+        quotes = response.xpath("//span[@class = 'text' and @itemprop= 'text']/text()").getall()
 
-        topTenTags = response.xpath('//div/span/a[@class="tag"]/text()').getall()
+        topTags = response.xpath('//div/span/a[@class="tag"]/text()').getall()
+
+        top = getattr(self,'top', None)
+        if top:
+            top = int(top)
+            topTags = topTags[:top]
+        """
+        Si existe dentro de la ejecución de este spider un atributo de nombre
+        top lo voy a guardar en mi variable top. Si no se envía el atributo en 
+        la ejecución se guarda None en top
+        scrapy crawl quotes -a top=1 
+        """    
+
         yield {
                 'titles': title,
-                'quotes': citas,
-                'top ten tags': topTenTags
+                'top tags': topTags
             }
 
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
     
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse)
-        """
-        El metodo follow sigue el nuevo link, luego de que se hace la request   
-        Este metodo posee un callback que ejecutara el metodo parse con el nuevo response
-        """
+            yield response.follow(next_page_button_link, 
+            callback=self.parse_only_quotes, cb_kwargs= {'quotes': quotes})
+    """
+    1. To understand callback: A callback function is a function passed into another function as an argument, 
+    which is then invoked inside the outer function to complete some kind of routine or action.
+    
+    2.To understand cb_kwargs: https://stackoverflow.com/questions/30695803/what-does-cb-in-cb-kwargs-stand-for
+    """   
